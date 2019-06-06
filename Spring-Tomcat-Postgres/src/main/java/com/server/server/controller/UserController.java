62f3d086d4bea123ed04a4eb1884559f0a20e068
Nathan Thomas <nwthomas@me.com>
2019-06-06 10:39:56 -0700

    Create a single user by id .get() request route

--- Spring-Tomcat-Postgres/src/main/java/com/server/server/controller/UserController.java
+++ Spring-Tomcat-Postgres/src/main/java/com/server/server/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.server.server.controller;
 
 import com.server.server.ServerApplication;
+import com.server.server.exception.ResourceNotFoundException;
+import com.server.server.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,19 @@ public class UserController
 	}
 
 	// /users/{id}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> getSingleUser(@PathVariable int id)
+	{
+		User rtnUser = ServerApplication.ourUserList.findUser(u -> u.getId() == id);
+		if (rtnUser == null)
+		{
+			throw new ResourceNotFoundException("User with id " + id + " is not found.");
+		}
+		else
+		{
+			return new ResponseEntity<>(rtnUser, HttpStatus.OK);
+		}
+	}
 
 	// /users/{letter}
 

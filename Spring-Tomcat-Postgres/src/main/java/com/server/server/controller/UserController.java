3560a8615c000302859714efe439f3398bc8321d
Nathan Thomas <nwthomas@me.com>
2019-06-06 10:35:49 -0700

    Build out /users/all .get() route

--- Spring-Tomcat-Postgres/src/main/java/com/server/server/controller/UserController.java
+++ Spring-Tomcat-Postgres/src/main/java/com/server/server/controller/UserController.java
@@ -1,5 +1,28 @@
 package com.server.server.controller;
 
+import com.server.server.ServerApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController
 {
+	// /users/all
+	@GetMapping(value = "/all", produces = {"application/json"})
+	public ResponseEntity<?> getallUsers()
+	{
+		return new ResponseEntity<>(ServerApplication.ourUserList.userList, HttpStatus.OK);
+	}
+
+	// /users/{id}
+
+	// /users/{letter}
+
+	// /users/{email}
+
+	// /users/{phone}
 }

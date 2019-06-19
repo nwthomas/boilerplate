5f2e9f3ce939861fe34648a2f5463acfa156fb2a
Nathan Thomas <nwthomas@me.com>
2019-06-18 23:21:08 -0700

    Update UserService.java interface with .findAll() and .delete()

--- Spring-Tomcat-Postgres/src/main/java/com/server/server/services/UserService.java
+++ Spring-Tomcat-Postgres/src/main/java/com/server/server/services/UserService.java
@@ -1,5 +1,12 @@
 package com.server.server.services;
 
+import com.server.server.model.User;
+
+import java.util.ArrayList;
+
 public interface UserService
 {
+	ArrayList<User> findAll();
+
+	void delete(long id);
 }
